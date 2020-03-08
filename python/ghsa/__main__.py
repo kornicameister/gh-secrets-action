0457e1184e596304c1beccf029d6803a02b11569
Tomasz Trębski <kornicameister@gmail.com>
2020-03-08 22:53:11 +0100

    Add getting encryption key + putting secret

--- python/ghsa/__main__.py
+++ python/ghsa/__main__.py
@@ -11,30 +11,59 @@ def main(
         gh_repository: str,
         gh_token: str,
 ) -> None:
-    ...
-
-
-def encrypt(p_key: str, secret_value: str) -> str:
-    public_key = public.PublicKey(
-        p_key.encode('utf-8'),
-        encoding.Base64Encoder(),
+    encryption_key, encryption_key_id = get_encryption_key(
+        gh_repository,
+        gh_token,
     )
+    encrypted_secrets = {
+        k: encrypt_using_key(v, key=encryption_key)
+        for k, v in secrets.items()
+    }
+    for secret_name, secret_value in encrypted_secrets:
+        # PUT /repos/:owner/:repo/actions/secrets/:name
+        r.put(
+            (
+                f'https://api.github.com/repos/{gh_repository}/'
+                f'actions/secrets/{secret_name}'
+            ),
+            headers={
+                'Authorization': f'token {gh_token}',
+            },
+            json={
+                'key_id': encryption_key_id,
+                'encrypted_value': secret_value,
+            },
+        )
 
-    sealed_box = public.SealedBox(public_key)
-    encrypted = sealed_box.encrypt(secret_value.encode('utf-8'))
+
+def encrypt_using_key(
+        value: str,
+        key: str,
+) -> str:
+    sealed_box = public.SealedBox(
+        public.PublicKey(
+            key.encode('utf-8'),
+            encoding.Base64Encoder(),
+        ),
+    )
+    encrypted = sealed_box.encrypt(value.encode('utf-8'))
 
     return base64.b64encode(encrypted).decode('utf-8')
 
 
-def get_secret_public_key(gh_repo: str, gh_token: str) -> t.Dict[str, str]:
+def get_encryption_key(
+        gh_repo: str,
+        gh_token: str,
+) -> t.Tuple[str, str]:
     # GET /repos/:owner/:repo/actions/secrets/public-key
-    return r.get(
+    public_key = r.get(
         f'https://api.github.com/repos/{gh_repo}/actions/secrets/public-key',
         headers={
             'Authorization': f'token {gh_token}',
             'Content-Type': 'application/json',
         },
     ).json()
+    return public_key['key'], public_key['key_id']
 
 
 if __name__ == '__main__':
