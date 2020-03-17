d12b7e227fb8c3e4eb2a102204b24e83a945673e
Tomasz Trębski <kornicameister@gmail.com>
2020-03-17 21:48:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
-env | grep -i INPUT_SECRET-
-
 python -m ghsa \
-  "${INPUT_GH-SECRETS}" \
+  "${INPUT_SECRETS}" \
   --repo "${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}" \
   --token "${INPUT_TOKEN}"
